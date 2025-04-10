0a6aa4e90f8a410e77497f06cc82df132bf3565e
yasinylz <ysn.yldzzza@gmail.com>
2025-04-10 23:49:11 +0300

    navbar, slider and dark mode adjusted

---
+++ Modello-Store/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './component/Navbar/Navbar'
+import Hero from './component/Hero/Hero'
+import Products from './component/Products/Products'
+
+function App() {
+  return (
+    <div >
+ <Navbar/>
+ <Hero/>
+ <Products/>
+    </div>
+  )
+}
+
+export default App
---
+++ Modello-Store/src/component/Products/Products.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <div className="mt-14 mb-12">
+        <div className="container">
+            {/*Header section */}
+            <div className="text-center mb-10 max-w-[600px] mx-auto">
+                <p className="text-sm text-primary">Top Selling Products For You</p>
+                <h1 className="text-3xl font-bold">Products</h1>
+                <p className="text-xs text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis nesciunt quaerat ullam dolores. Exercitationem nisi obcaecati amet nemo dolore eligendi eius dicta asperiores ullam beatae provident deserunt dolorem, officiis accusamus.</p>
+            </div> 
+            {/*Body section */}
+        </div>
+      
+    </div>
+  )
+}
+
+export default Products
