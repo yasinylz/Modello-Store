9d9af1d7fb4840de56b872880bbe058a1e2c6fcf
yasinylz <ysn.yldzzza@gmail.com>
2025-04-14 19:43:49 +0300

    proje  deploy  için hazır

--- Modello-Store/vite.config.js
+++ Modello-Store/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100, // polling sıklığı (ms)
+    },
+  },
 })
