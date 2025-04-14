8acee6038e9d71134baf1487ce13d820dc64e0ac
yasinylz <ysn.yldzzza@gmail.com>
2025-04-14 19:40:14 +0300

    proje  deploy  için hazır

--- Modello-Store/src/App.jsx
+++ Modello-Store/src/App.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
-import Navbar from './component/Navbar/Navbar'
-import Hero from './component/Hero/Hero'
-import Products from './component/Products/Products'
+import React from "react";
+import Navbar from "./component/Navbar/Navbar";
+import Hero from "./component/Hero/Hero";
+import Products from "./component/Products/Products";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import TopProducts from "./component/TopProducts/TopProducts";
+import Banner from "./component/Banner/Banner";
+import Subscribe from "./component/Subscribe/Subscribe";
+import Testimonials from "./component/Testimonials/Testimonials";
+import Footer from "./component/Footer/Footer";
+import Popup from "./component/Popup/Popup";
 
-function App() {
+const App = () => {
+  const [orderPopup, setOrderPopup] = React.useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
-    <div >
- <Navbar/>
- <Hero/>
- <Products/>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+      <Navbar handleOrderPopup={handleOrderPopup} />
+      <Hero handleOrderPopup={handleOrderPopup} />
+      <Products />
+      <TopProducts handleOrderPopup={handleOrderPopup} />
+      <Banner />
+      <Subscribe />
+      <Products />
+      <Testimonials />
+      <Footer />
+      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
