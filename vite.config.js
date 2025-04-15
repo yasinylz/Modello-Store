83b452b15e7eb276f69202dc29ba6a1dae8f4d0c
yasinylz <ysn.yldzzza@gmail.com>
2025-04-15 22:47:46 +0300

    ,

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Modello-Store/', // 👈 BU SATIR BİZİM ALTIN ANAHTAR
   server: {
     watch: {
       usePolling: true,
